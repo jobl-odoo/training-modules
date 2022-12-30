694236288c9fa84364537b1a09d0b2bf686b36c2
jobl <jobl@odoo.com>
2022-12-30 23:46:57 +0000

    [ADD] Basic Module

---
+++ space_mission/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Space Mission',
+    'summary': 'Logistics app to manage space mission',
+    'description': """
+        Space Mission app:
+        - Organizes logistics
+        - Includes Spaceship and Space Crew
+    """,
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com',
+    'category': 'Logistics',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': [
+    ],
+}
