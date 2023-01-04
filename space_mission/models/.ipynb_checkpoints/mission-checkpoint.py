057a4eb529560c2c5fa12d7f9195dba8e52936b8
jobl-odoo <jobl@odoo.com>
2023-01-04 23:30:52 +0000

    [ADD] Graph view for missions, mission duration field

--- space_mission/models/.ipynb_checkpoints/mission-checkpoint.py
+++ space_mission/models/.ipynb_checkpoints/mission-checkpoint.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import timedelta
 
 class Mission(models.Model):
     
@@ -17,5 +18,22 @@ class Mission(models.Model):
                                     string='Crew Members')
     launch_date = fields.Date(string='Launch Date')
     return_date = fields.Date(string='Return Date')
-                                 
-                                 
+    fuel_required_in_t = fields.Integer(string='Fuel Required in Tons')
+    num_engines = fields.Integer(string='Number of Engines')
+    safety_features = fields.Selection(string='Safety Features',
+                                       selection=[('low', 'Low'),
+                                                  ('medium', 'Medium'),
+                                                  ('high', 'High')])
+    
+    mission_duration = fields.Integer(string='Mission Length in Days',
+                                      compute='_compute_mission_duration',
+                                      store=True)
+    
+    @api.depends('launch_date', 'return_date')
+    def _compute_mission_duration(self):
+        for record in self:
+            if not (record.launch_date and record.return_date):
+                record.mission_duration = 0
+            else:
+                duration = record.return_date - record.launch_date
+                record.mission_duration = duration.days
