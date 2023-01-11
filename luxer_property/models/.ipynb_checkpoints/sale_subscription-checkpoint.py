43203e2212e1f49192f169cbcf1e2564fd311da1
jobl-odoo <jobl@odoo.com>
2023-01-11 20:24:16 +0000

    [Edit] Finalize onchange func to swap delivery address for property partner

--- luxer_property/models/.ipynb_checkpoints/sale_subscription-checkpoint.py
+++ luxer_property/models/.ipynb_checkpoints/sale_subscription-checkpoint.py
@@ -8,3 +8,13 @@ class SaleSubscription(models.Model):
     
     property_partner_id = fields.Many2one(comodel_name='res.partner',
                                        string='Property Partner')
+    
+    
+    @api.onchange('property_partner_id')
+    def change_delivery_address_to_pp(self):
+        """If Property Partner is set, update delivery address to match."""
+        
+        if self.property_partner_id:
+            self.partner_shipping_id = self.property_partner_id
+        elif self.partner_id:
+            self.partner_shipping_id = self.partner_id
