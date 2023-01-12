51769f8f5b87416c2bc0b7bb949e81c86e71a8bb
jobl-odoo <jobl@odoo.com>
2023-01-12 23:57:45 +0000

    [ADD] Sequence for barcode

--- pg_barcode/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ pg_barcode/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -6,6 +6,7 @@
     'description': """
         Adjusts barcode based on new Product Group field
         - first 2 digits of Product Group added to barcode
+        - barcode from 6 digit sequence beginning with 900000
         - XX.barcode
         
         Task ID: 3091248""",
@@ -18,5 +19,7 @@
         'stock_barcode'
     ],
     'data': [
+        'views/product_template_views_inherit.xml',
+        'data/product_template_sequence.xml',
     ],
 }
--- pg_barcode/models/product_template.py
+++ pg_barcode/models/product_template.py
@@ -9,4 +9,21 @@ class ProductTemplate(models.Model):
     product_group = fields.Selection(string='Product Group',
                                      selection=[('1234', '1234'),
                                                 ('2345', '2345'),
-                                                ('3456', '3456')])
+                                                ('3456', '3456')],
+                                     required=True,
+                                     default='1234')
+    
+    
+    @api.onchange('product_group')
+    def add_pg_to_barcode(self):
+        """Prefixes first 2 digits of product group to barcode:
+            Product Code = '1234' => Barcode = '12.XXXXXX'
+            Barcodes generated from sequence beginning with 900000."""
+        
+        for record in self:
+            if record.barcode:
+                original_barcode = record.barcode.split('.')[-1]
+            else:
+                original_barcode = self.env['ir.sequence'].next_by_code('pg_barcode.barcode')
+            if record.product_group:
+                record.write({'barcode': f'{record.product_group[:2]}.{original_barcode}'})
