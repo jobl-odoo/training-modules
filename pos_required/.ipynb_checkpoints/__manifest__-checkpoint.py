24c0188393e6cdf811e83f0c0d29ca066600bbcd
jobl-odoo <jobl@odoo.com>
2023-01-12 18:06:21 +0000

    [ADD] Add module with required fields in POS

---
+++ pos_required/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'POS Required Fields',
+    'summary': """Set VAT and Phone to be required fields in POS""",
+    'description': """
+        When creating a new client, set the additional following fields
+        to be required
+        - VAT (Tax ID)
+        - Phone
+        
+        Task ID: 3091247""",
+    'author': 'Odoo Inc',
+    'website': 'https://www.odoo.com',
+    'license': 'OPL-1',
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['pos'],
+    'data': [],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_required/static/src/js/ClientDetailsEdit.js',
+        ]
+    }
+}
