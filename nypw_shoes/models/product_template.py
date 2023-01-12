8fe98cdb6c2fdc52d571ad68b7b42855739a2723
jobl-odoo <jobl@odoo.com>
2023-01-12 00:03:16 +0000

    [ADD] Readonly modifiers based on filled in fields

--- nypw_shoes/models/product_template.py
+++ nypw_shoes/models/product_template.py
@@ -9,3 +9,12 @@ class ProductTemplate(models.Model):
     pairs_per_case = fields.Integer(string='Pairs per Case')
     
     price_per_pair = fields.Monetary(string='Price per Pair')
+    
+    
+    @api.onchange('pairs_per_case', 'price_per_pair')
+    def _calc_sales_price(self):
+        """Calculate Sales Price based on new fields."""
+        
+        for record in self:
+            if record.pairs_per_case and record.price_per_pair:
+                record.list_price = record.pairs_per_case * record.price_per_pair
