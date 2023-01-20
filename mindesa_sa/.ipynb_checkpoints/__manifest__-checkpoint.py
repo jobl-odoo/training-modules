6b47f96fe324b4dac144534707408c7a1158c1d3
jobl-odoo <jobl@odoo.com>
2023-01-20 17:40:51 +0000

    [ADD] Base module and model

---
+++ mindesa_sa/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Mindesa Scheduled RFQ Confirm',
+    'summary': """Confirms all RFQs each night made to central store""",
+    'description': """
+        Scheduled Action to auto-confirm RFQs made to central store
+        - Adds method to purchase.order model
+        - Adds scheduled action to call method
+        
+        Task ID: 3091209""",
+    'author': 'Odoo Inc',
+    'website': 'https://www.odoo.com',
+    'license': 'OPL-1',
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': [
+        'purchase',
+        'inventory',
+        'contacts',
+    ],
+    'data': [
+    ],
+}
